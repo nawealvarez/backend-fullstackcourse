80013dd64adca83456d2bac05de5f13be9661267
Nahuel <nawe7depb@gmail.com>
2020-06-14 19:29:19 -0300

    more fixes in index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-require('dotenv').config(path: __dirname + '/.env');
+require('dotenv').config({path: __dirname + '/.env'});
 const Person = require('./models/person');
 const morgan = require('morgan');
 
